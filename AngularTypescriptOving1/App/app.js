5dacfa85c4d1f65b6977013b5611239d28fc9ba7
esbenUnipluss <esben@unipluss.no>
2015-03-09 11:28:12 +0100

    Recomit

--- AngularTypescriptOving1/App/app.js
+++ AngularTypescriptOving1/App/app.js
@@ -7,34 +7,6 @@ var app = angular.module('crimeWatcherApp', [
 ]).config(['$routeProvider', function ($routeProvider) {
     $routeProvider.otherwise({ redirectTo: '/CrimeCategories' });
 }]);
-//angular.module('crimeWatcherApp.DatePicker', ['ngRoute', 'ui.bootstrap'])
-//    .controller('DatepickerDemoCtrl', function ($scope) {
-//    $scope.today = function () {
-//        $scope.dt = new Date();
-//    };
-//    $scope.today();
-//    $scope.clear = function () {
-//        $scope.dt = null;
-//    };
-//    $scope.disabled = function (date, mode) {
-//        return false;
-//    };
-//    $scope.toggleMin = function () {
-//        $scope.minDate =new Date(2000,1,1);
-//    };
-//    $scope.toggleMin();
-//    $scope.open = function ($event) {
-//        $event.preventDefault();
-//        $event.stopPropagation();
-//        $scope.opened = true;
-//    };
-//    $scope.dateOptions = {
-//        formatYear: 'yy',
-//        startingDay: 1
-//    };
-//    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-//    $scope.format = $scope.formats[3];
-//}); 
 angular.module('crimeWatcherApp.CrimeCategories', ['ngRoute']).config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/CrimeCategories', {
         templateUrl: 'App/CrimeCategories/CrimeCategories.html',
